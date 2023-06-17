14a7c5c02179d63457b82e19cb9403eb35095ec9
[Brendan Chermack] <101033479+BrendanChermack@users.noreply.github.com>
2023-06-17 15:16:04 -0500

    reinstall commit for initial files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+const App = () => {
+return (
+		<div className="App">
+			<nav className='navBar'>
+			<ul>
+				<li><a href='#'>Home</a></li>
+				<li><a href='#'>Todo</a></li>
+				<li><a href='#'>Help</a></li>
+			</ul>
+			</nav>
+		</div>
+  );
+}
+
+export default App;
